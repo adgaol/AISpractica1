7444684303825211d82e520e3d0ab8727ace49cb
aunionirene <37622144+aunionirene@users.noreply.github.com>
2018-05-21 19:56:29 +0200

    Add files via upload

--- tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/boardTest.java
+++ tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/boardTest.java
@@ -16,51 +16,101 @@ import org.junit.Before;
 import es.codeurjc.ais.tictactoe.Board;
 import es.codeurjc.ais.tictactoe.Player;
 import es.codeurjc.ais.tictactoe.TicTacToeGame;
+
 import static org.hamcrest.core.IsEqual.equalTo;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 public class boardTest {
 	private Player jugador1;
 	private Player jugador2;
 	private Board tablero;
-	private TicTacToeGame juego;
-	/*@Parameters
-	public static Collection<Object[]> data() {
-	Object[][] values = {
-			{ 2, 3, 0, 4, 1, 5 },
-			{ 3, 2, 4, 0, 5, 1 }
-	};
-	return Arrays.asList(values);
-	}
-	@Parameter(0) public int moveOne;*/
+	
 	@Before
 	public void setUp() {
 		jugador1=new Player(1, "pepe", "pepe");
 		jugador2=new Player(2, "manolo", "manolo");
 		tablero=new Board();
-		juego=new TicTacToeGame();
-		juego.addPlayer(jugador1);
-		juego.addPlayer(jugador2);
-                tablero.enableAll();
+		tablero.enableAll();
 	}
+	
 	@Test
-	public void ifwin() {
-                juego.mark(2);
-		juego.mark(3);
-		juego.mark(0);
-		juego.mark(4);
-		juego.mark(1);
-		int[] introducidos = new int[3];
-		introducidos[0]=2;
-		introducidos[1]=0;
-		introducidos[2]=1;
+	public void winTest() {
+		//Jugador1 gana
+		tablero.getCell(0).value=jugador1.getLabel();
+		tablero.getCell(0).active=false;
+		tablero.getCell(3).value=jugador2.getLabel();
+		tablero.getCell(3).active=false;
+		tablero.getCell(1).value=jugador1.getLabel();
+		tablero.getCell(1).active=false;
+		tablero.getCell(4).value=jugador2.getLabel();
+		tablero.getCell(4).active=false;
+		tablero.getCell(2).value=jugador1.getLabel();
+		tablero.getCell(2).active=false;
+
+		int[] introducidos = {0,1,2};
+		assertThat(introducidos,equalTo(tablero.getCellsIfWinner(jugador1.getLabel())));
+		assertThat(null,equalTo(tablero.getCellsIfWinner(jugador2.getLabel())));
+		
+		assertThat(false,equalTo(tablero.checkDraw()));
+	}
+	
+	@Test
+	public void loseTest() {
+		//Jugador1 pierde
+		tablero.getCell(3).value=jugador1.getLabel();
+		tablero.getCell(3).active=false;
+		tablero.getCell(0).value=jugador2.getLabel();
+		tablero.getCell(0).active=false;
+		tablero.getCell(6).value=jugador1.getLabel();
+		tablero.getCell(6).active=false;
+		tablero.getCell(4).value=jugador2.getLabel();
+		tablero.getCell(4).active=false;
+		tablero.getCell(1).value=jugador1.getLabel();
+		tablero.getCell(1).active=false;
+		tablero.getCell(7).value=jugador2.getLabel();
+		tablero.getCell(7).active=false;
+		tablero.getCell(2).value=jugador1.getLabel();
+		tablero.getCell(2).active=false;
+		tablero.getCell(8).value=jugador2.getLabel();
+		tablero.getCell(8).active=false;
                 
-		int [] tableroGanado=tablero.getCellsIfWinner(jugador2.getLabel());
-                /*System.out.println(tableroGanado==null);*/
-		for(int i=0;i<tableroGanado.length;i++) {
-			assertThat(introducidos[i],equalTo(tableroGanado[i]));
-		}
+		int[] introducidos = {0,4,8};
+		assertThat(null,equalTo(tablero.getCellsIfWinner(jugador1.getLabel())));
+		assertThat(introducidos,equalTo(tablero.getCellsIfWinner(jugador2.getLabel())));
+		
+		assertThat(false,equalTo(tablero.checkDraw()));
+	}
+	
+	@Test
+	public void drawTest() {
+		//Empate
+		tablero.getCell(0).value=jugador1.getLabel();
+		tablero.getCell(0).active=false;
+		tablero.getCell(2).value=jugador2.getLabel();
+		tablero.getCell(2).active=false;
+		tablero.getCell(1).value=jugador1.getLabel();
+		tablero.getCell(1).active=false;
+		tablero.getCell(3).value=jugador2.getLabel();
+		tablero.getCell(3).active=false;
+		tablero.getCell(5).value=jugador1.getLabel();
+		tablero.getCell(5).active=false;
+		tablero.getCell(4).value=jugador2.getLabel();
+		tablero.getCell(4).active=false;
+		tablero.getCell(6).value=jugador1.getLabel();
+		tablero.getCell(6).active=false;
+		tablero.getCell(7).value=jugador2.getLabel();
+		tablero.getCell(7).active=false;
+		tablero.getCell(8).value=jugador1.getLabel();
+		tablero.getCell(8).active=false;
+		
+		assertThat(null,equalTo(tablero.getCellsIfWinner(jugador1.getLabel())));
+		assertThat(null,equalTo(tablero.getCellsIfWinner(jugador2.getLabel())));
+		
+		assertThat(true,equalTo(tablero.checkDraw()));
 	}
 
 }
